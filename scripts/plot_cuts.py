4144f29172e688d88653f72be5a8824122fc8051
Richard Lane <richard.lane@bristol.ac.uk>
2022-07-25 11:51:43 +0100

    nicer plot of cuts

--- scripts/plot_cuts.py
+++ scripts/plot_cuts.py
@@ -13,21 +13,23 @@ sys.path.append(str(pathlib.Path(__file__).absolute().parents[1]))
 from lib_cuts import read_data, util
 
 
-def _plot_df(sig_df: pd.DataFrame, bkg_df: pd.DataFrame) -> None:
+def _plot_df(
+    sig_df: pd.DataFrame,
+    bkg_df: pd.DataFrame,
+    sig_predictions: np.ndarray,
+    bkg_predictions: np.ndarray,
+) -> None:
     """
     Plot and show stuff
 
     """
-    test_sig_df = sig_df[~sig_df["train"]]
-    test_bkg_df = bkg_df[~bkg_df["train"]]
-
     # Plot
     fig, ax = plt.subplots(5, 3, figsize=(15, 9))
+    sig_colour, bkg_colour = "b", "r"
 
-    kw = {"histtype": "step"}
     quantiles = [0.01, 0.99]  # Which quantiles to use for binning
     n_bins = 100
-    for col, axis in zip(test_sig_df, ax.ravel()):
+    for col, axis in zip(sig_df, ax.ravel()):
         bkg_quantile = np.quantile(bkg_df[col], quantiles)
         sig_quantile = np.quantile(sig_df[col], quantiles)
         bins = np.linspace(
@@ -36,18 +38,38 @@ def _plot_df(sig_df: pd.DataFrame, bkg_df: pd.DataFrame) -> None:
             n_bins,
         )
 
-        axis.hist(test_sig_df[col], bins=bins, **kw, label="sig")
-        axis.hist(test_bkg_df[col], bins=bins, **kw, label="bkg")
+        # Plot before cuts
+        axis.hist(
+            sig_df[col], bins=bins, histtype="step", label="sig", color=sig_colour
+        )
+        axis.hist(
+            bkg_df[col], bins=bins, histtype="step", label="bkg", color=bkg_colour
+        )
+
+        # Plot after cuts
+        axis.hist(
+            sig_df[col][sig_predictions == 1],
+            bins=bins,
+            alpha=0.5,
+            color=sig_colour,
+        )
+        axis.hist(
+            bkg_df[col][bkg_predictions == 1],
+            bins=bins,
+            alpha=0.5,
+            color=bkg_colour,
+        )
+
         axis.set_title(col)
 
-        # Add a * to the plots not used in training the classifier
+        # Add a * to the plot titles for those not used in training the classifier
         if col not in read_data.training_var_names():
             title = axis.get_title()
             axis.set_title(title + "*")
 
         axis.legend()
 
-    fig.suptitle("Test sample")
+    fig.suptitle("Testing sample")
     fig.tight_layout()
     plt.show()
 
@@ -61,14 +83,17 @@ def main():
     sig_df = util.read_dataframe(background=False)
     bkg_df = util.read_dataframe(background=True)
 
-    _plot_df(sig_df, bkg_df)
+    # We only want the testing data here
+    sig_df = sig_df[~sig_df["train"]]
+    bkg_df = bkg_df[~bkg_df["train"]]
 
-    # Now make the plots but remove the bkg
+    # Predict which of these are signal and background using our classifier
     clf = util.get_classifier()
-    sig_labels = clf.predict(sig_df[list(read_data.training_var_names())])
-    bkg_labels = clf.predict(bkg_df[list(read_data.training_var_names())])
+    sig_predictions = clf.predict(sig_df[list(read_data.training_var_names())])
+    bkg_predictions = clf.predict(bkg_df[list(read_data.training_var_names())])
 
-    _plot_df(sig_df[sig_labels == 1], bkg_df[bkg_labels == 1])
+    # Plot histograms of our variables before/after doing these cuts
+    _plot_df(sig_df, bkg_df, sig_predictions, bkg_predictions)
 
 
 if __name__ == "__main__":
