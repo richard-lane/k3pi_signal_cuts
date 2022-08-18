90c047354903c575abca87d9f4f86be5b79e0f49
Richard Lane <richard.lane@bristol.ac.uk>
2022-08-18 17:28:27 +0100

    roc

--- scripts/plot_roc.py
+++ scripts/plot_roc.py
@@ -9,19 +9,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, roc_auc_score
 
+sys.path.append(str(pathlib.Path(__file__).absolute().parents[2] / "k3pi-data"))
 sys.path.append(str(pathlib.Path(__file__).absolute().parents[1]))
 
-from lib_cuts import read_data, util
+from lib_cuts.get import classifier as get_clf
+from lib_data import get, training_vars
 
 
 def main():
     """
-    Show plots before and after applying cuts with the classifier
+    ROC curve
 
     """
     # Read dataframes of stuff
-    sig_df = util.read_dataframe(background=False)
-    bkg_df = util.read_dataframe(background=True)
+    year, sign, magnetisation = "2018", "dcs", "magdown"
+    sig_df = get.mc(year, sign, magnetisation)
+    bkg_df = pd.concat(get.uppermass(year, sign, magnetisation))
 
     # We only want the testing data here
     sig_df = sig_df[~sig_df["train"]]
@@ -30,8 +33,9 @@ def main():
     combined_df = pd.concat((sig_df, bkg_df))
     combined_y = np.concatenate((np.ones(len(sig_df)), np.zeros(len(bkg_df))))
 
-    predicted_probs = util.get_classifier().predict_proba(
-        combined_df[list(read_data.training_var_names())]
+    training_labels = list(training_vars.training_var_names())
+    predicted_probs = get_clf(year, sign, magnetisation).predict_proba(
+        combined_df[training_labels]
     )[:, 1]
     fpr, tpr, _ = roc_curve(combined_y, predicted_probs)
     score = roc_auc_score(combined_y, predicted_probs)
@@ -43,6 +47,8 @@ def main():
     ax.set_ylabel("true positive rate")
     ax.legend()
     fig.tight_layout()
+
+    plt.savefig("roc.png")
 
     plt.show()
 
