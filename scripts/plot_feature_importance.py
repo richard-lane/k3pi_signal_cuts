adea4e154939556ae37669f574e71d388cbf5f52
Richard Lane <richard.lane@bristol.ac.uk>
2022-09-08 13:16:38 +0100

    revive feature importance script

--- scripts/plot_feature_importance.py
+++ scripts/plot_feature_importance.py
@@ -7,8 +7,10 @@ import pathlib
 import matplotlib.pyplot as plt
 
 sys.path.append(str(pathlib.Path(__file__).absolute().parents[1]))
+sys.path.append(str(pathlib.Path(__file__).absolute().parents[2] / "k3pi-data"))
 
-from lib_cuts import util, read_data
+from lib_cuts.get import classifier as get_clf
+from lib_data import training_vars
 
 
 def main():
@@ -17,13 +19,13 @@ def main():
 
     """
     # Predict which of these are signal and background using our classifier
-    clf = util.get_classifier()
+    clf = get_clf("2018", "dcs", "magdown")
 
     fig, ax = plt.subplots(figsize=(10, 5))
 
     importances = clf.feature_importances_
     ax.bar(range(len(importances)), importances)
-    ax.set_xticks(range(len(importances)), read_data.training_var_names(), rotation=45)
+    ax.set_xticks(range(len(importances)), training_vars.training_var_names(), rotation=45)
 
     fig.suptitle("Feature Importances")
     fig.tight_layout()
